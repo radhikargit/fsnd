69e01be9dff806f41de95dc19d67aa58fa9b9e96
radhikargit <radhikamreddy@gmail.com>
2021-10-10 23:11:15 -0700

    reading credentials from environment var

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -14,10 +14,13 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        user_name = "postgres"
-        password = "postgres"
-        self.database_path = "postgresql://{}:{}@{}/{}".format(user_name, password, "localhost:5432", self.database_name)
+        DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')  
+        DB_USER = os.getenv('DB_USER', 'postgres')  
+        DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')  
+        DB_NAME = os.getenv('DB_NAME', 'trivia')  
+        database_path = 'postgresql://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+        self.database_name = DB_NAME
+        self.database_path = database_path
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
